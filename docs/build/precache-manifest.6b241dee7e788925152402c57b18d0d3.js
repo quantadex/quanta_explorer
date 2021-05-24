75388eb6a5898df757f605dded421152b6bdf370
Quoc <quocble@gmail.com>
2021-05-24 13:01:30 -0400

    fixed path

---
+++ docs/build/precache-manifest.6b241dee7e788925152402c57b18d0d3.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d2c08d045b15817c4e9",
+    "url": "/quanta_explorer/static/js/19.4d2c08d0.chunk.js"
+  },
+  {
+    "revision": "db6496fbd6734b92eb11",
+    "url": "/quanta_explorer/static/js/0.db6496fb.chunk.js"
+  },
+  {
+    "revision": "67e97531bc9eeada12ba",
+    "url": "/quanta_explorer/static/js/2.67e97531.chunk.js"
+  },
+  {
+    "revision": "29282c7e1198feaf6afb",
+    "url": "/quanta_explorer/static/js/3.29282c7e.chunk.js"
+  },
+  {
+    "revision": "62f2c7ac7a392faefc4b",
+    "url": "/quanta_explorer/static/js/4.62f2c7ac.chunk.js"
+  },
+  {
+    "revision": "d81777ebb61bc91c1da3",
+    "url": "/quanta_explorer/static/js/5.d81777eb.chunk.js"
+  },
+  {
+    "revision": "1706ce20cdc47e899371dc16f2775937",
+    "url": "/quanta_explorer/static/media/medium.1706ce20.svg"
+  },
+  {
+    "revision": "66e0159f88905f76c4c9",
+    "url": "/quanta_explorer/static/js/6.66e0159f.chunk.js"
+  },
+  {
+    "revision": "fa6c1f804e222bc6c57e66aa2becbcd8",
+    "url": "/quanta_explorer/static/media/telegram.fa6c1f80.svg"
+  },
+  {
+    "revision": "2ca91b3a3068454cfee0",
+    "url": "/quanta_explorer/static/js/main.2ca91b3a.chunk.js"
+  },
+  {
+    "revision": "d3c15752e39c9d122e18fa9a487d0d20",
+    "url": "/quanta_explorer/static/media/linkedin.d3c15752.svg"
+  },
+  {
+    "revision": "76feac6a39ed3f103f48",
+    "url": "/quanta_explorer/static/js/8.76feac6a.chunk.js"
+  },
+  {
+    "revision": "dbbd0a67216ca0342d4b77b3a993f44d",
+    "url": "/quanta_explorer/static/media/twitter.dbbd0a67.svg"
+  },
+  {
+    "revision": "bda931837d0e9f974ad5",
+    "url": "/quanta_explorer/static/js/9.bda93183.chunk.js"
+  },
+  {
+    "revision": "aa48a925fd6bc775dc2ab5e60406267d",
+    "url": "/quanta_explorer/static/media/facebook.aa48a925.svg"
+  },
+  {
+    "revision": "3516d92e32f86e0cf490",
+    "url": "/quanta_explorer/static/js/10.3516d92e.chunk.js"
+  },
+  {
+    "revision": "d0f348e52d465018ba59c09c736a8892",
+    "url": "/quanta_explorer/static/media/down_arrow.d0f348e5.svg"
+  },
+  {
+    "revision": "eb126e4024075eb047f3",
+    "url": "/quanta_explorer/static/js/11.eb126e40.chunk.js"
+  },
+  {
+    "revision": "7d112f9de2180f76cf8cc7d81c32d38b",
+    "url": "/quanta_explorer/static/media/logo.7d112f9d.svg"
+  },
+  {
+    "revision": "dc6bc9c5743aa1417110",
+    "url": "/quanta_explorer/static/js/12.dc6bc9c5.chunk.js"
+  },
+  {
+    "revision": "3668c739ea9cfc66774beeec9dd20d7a",
+    "url": "/quanta_explorer/static/media/caution.3668c739.svg"
+  },
+  {
+    "revision": "14a963f5461d4fb485db",
+    "url": "/quanta_explorer/static/js/13.14a963f5.chunk.js"
+  },
+  {
+    "revision": "321c02619a788c433328",
+    "url": "/quanta_explorer/static/js/runtime~main.321c0261.js"
+  },
+  {
+    "revision": "fc43df5f1dcbb5de4268",
+    "url": "/quanta_explorer/static/js/14.fc43df5f.chunk.js"
+  },
+  {
+    "revision": "20a70220d0ab948708db",
+    "url": "/quanta_explorer/static/js/15.20a70220.chunk.js"
+  },
+  {
+    "revision": "b858b2bef827b4eb57d8",
+    "url": "/quanta_explorer/static/js/16.b858b2be.chunk.js"
+  },
+  {
+    "revision": "afbca84d0efe67697ef7",
+    "url": "/quanta_explorer/static/js/32.afbca84d.chunk.js"
+  },
+  {
+    "revision": "748e49681b0b94c03a0c",
+    "url": "/quanta_explorer/static/js/17.748e4968.chunk.js"
+  },
+  {
+    "revision": "ea7dbc203ea79418d9fd",
+    "url": "/quanta_explorer/static/js/31.ea7dbc20.chunk.js"
+  },
+  {
+    "revision": "f8fabea87277fea9f7e1",
+    "url": "/quanta_explorer/static/js/18.f8fabea8.chunk.js"
+  },
+  {
+    "revision": "e231002e105bf0ff2f9d",
+    "url": "/quanta_explorer/static/js/30.e231002e.chunk.js"
+  },
+  {
+    "revision": "5dc4ff9b2b4a93756fea",
+    "url": "/quanta_explorer/static/js/1.5dc4ff9b.chunk.js"
+  },
+  {
+    "revision": "4892ab81c90378ea4d6f",
+    "url": "/quanta_explorer/static/js/29.4892ab81.chunk.js"
+  },
+  {
+    "revision": "fa6b88081633fbf1a4b7",
+    "url": "/quanta_explorer/static/js/20.fa6b8808.chunk.js"
+  },
+  {
+    "revision": "b46aa365c382bcd6efe1",
+    "url": "/quanta_explorer/static/js/28.b46aa365.chunk.js"
+  },
+  {
+    "revision": "c8e27a0840ce39d04bbf",
+    "url": "/quanta_explorer/static/js/21.c8e27a08.chunk.js"
+  },
+  {
+    "revision": "44c8e8af180f9ad7c326",
+    "url": "/quanta_explorer/static/js/27.44c8e8af.chunk.js"
+  },
+  {
+    "revision": "5e1a4810a3c0592e37a1",
+    "url": "/quanta_explorer/static/js/22.5e1a4810.chunk.js"
+  },
+  {
+    "revision": "bc24b9087029bc57cace",
+    "url": "/quanta_explorer/static/js/26.bc24b908.chunk.js"
+  },
+  {
+    "revision": "1399330fff849597b327",
+    "url": "/quanta_explorer/static/js/23.1399330f.chunk.js"
+  },
+  {
+    "revision": "6f59cc91969bb0b2b86c",
+    "url": "/quanta_explorer/static/js/25.6f59cc91.chunk.js"
+  },
+  {
+    "revision": "1fe0e8f5c12a3565dead",
+    "url": "/quanta_explorer/static/js/24.1fe0e8f5.chunk.js"
+  },
+  {
+    "revision": "748e49681b0b94c03a0c",
+    "url": "/quanta_explorer/static/css/17.1813577e.chunk.css"
+  },
+  {
+    "revision": "6f59cc91969bb0b2b86c",
+    "url": "/quanta_explorer/static/css/25.84781aa6.chunk.css"
+  },
+  {
+    "revision": "bc24b9087029bc57cace",
+    "url": "/quanta_explorer/static/css/26.86159b90.chunk.css"
+  },
+  {
+    "revision": "66e0159f88905f76c4c9",
+    "url": "/quanta_explorer/static/css/6.5a14887c.chunk.css"
+  },
+  {
+    "revision": "44c8e8af180f9ad7c326",
+    "url": "/quanta_explorer/static/css/27.2584552a.chunk.css"
+  },
+  {
+    "revision": "2ca91b3a3068454cfee0",
+    "url": "/quanta_explorer/static/css/main.0c23112b.chunk.css"
+  },
+  {
+    "revision": "76feac6a39ed3f103f48",
+    "url": "/quanta_explorer/static/css/8.53b4d40f.chunk.css"
+  },
+  {
+    "revision": "bda931837d0e9f974ad5",
+    "url": "/quanta_explorer/static/css/9.38200032.chunk.css"
+  },
+  {
+    "revision": "eb126e4024075eb047f3",
+    "url": "/quanta_explorer/static/css/11.3b6a8904.chunk.css"
+  },
+  {
+    "revision": "4d2c08d045b15817c4e9",
+    "url": "/quanta_explorer/static/css/19.dc1d5747.chunk.css"
+  },
+  {
+    "revision": "1fe0e8f5c12a3565dead",
+    "url": "/quanta_explorer/static/css/24.a66bf680.chunk.css"
+  },
+  {
+    "revision": "1399330fff849597b327",
+    "url": "/quanta_explorer/static/css/23.fafb6799.chunk.css"
+  },
+  {
+    "revision": "5e1a4810a3c0592e37a1",
+    "url": "/quanta_explorer/static/css/22.ced208cc.chunk.css"
+  },
+  {
+    "revision": "c8e27a0840ce39d04bbf",
+    "url": "/quanta_explorer/static/css/21.6a3f5fcb.chunk.css"
+  },
+  {
+    "revision": "fa6b88081633fbf1a4b7",
+    "url": "/quanta_explorer/static/css/20.389c67c9.chunk.css"
+  },
+  {
+    "revision": "f8fabea87277fea9f7e1",
+    "url": "/quanta_explorer/static/css/18.cf50b2a9.chunk.css"
+  },
+  {
+    "revision": "fc43df5f1dcbb5de4268",
+    "url": "/quanta_explorer/static/css/14.95d4e132.chunk.css"
+  },
+  {
+    "revision": "14a963f5461d4fb485db",
+    "url": "/quanta_explorer/static/css/13.036c9cf3.chunk.css"
+  },
+  {
+    "revision": "dc6bc9c5743aa1417110",
+    "url": "/quanta_explorer/static/css/12.1520255b.chunk.css"
+  },
+  {
+    "revision": "3516d92e32f86e0cf490",
+    "url": "/quanta_explorer/static/css/10.7cf6ed93.chunk.css"
+  },
+  {
+    "revision": "a1adfa4fe7fa3761f0ddc3f7ca054770",
+    "url": "/quanta_explorer/index.html"
+  }
+];
